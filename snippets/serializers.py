cddb32f428e9e3cf0758cadeb034d9170b3031cd
jradd <jredd42@gmail.com>
2014-03-11 18:17:13 -0600

    DRF Snippets Tutorial complete

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,27 +1,23 @@
-from django.forms import widgets
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight',
+        format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style')
 
-    def restore_object(self, attrs, instance=None):
-        """
-        Create/Update a new snippet instance given a dict of
-        deserialized values.
-        """
 
-        if instance:
-            # Update existing instance.
-            instance.title = attrs.get('title', instance.title)
-            instance.code = attrs.get('code', instance.code)
-            instance.linenos = attrs.get('linenos', instance.linenos)
-            instance.language = attrs.get('language', instance.language)
-            instance.style = attrs.getA('style', instance.style)
-            return instance
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(view_name='snippets-detail',
+        many=True)
 
-        # Create new instance.
-        return Snippet(**attrs)
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
